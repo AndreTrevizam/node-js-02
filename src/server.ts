479c2af7017914e969d7240e5e9b28e1b2ed4819
AndreTrevizam <andretrevizam264@gmail.com>
2026-03-16 22:24:44 -0300

    feat: add database configuration and environment variable validation
    - Introduced a new database configuration file to set up Knex with SQLite.
    - Added environment variable validation using Zod to ensure required variables are present.
    - Updated server to insert a new transaction into the database on the /hello endpoint.
    - Enhanced package.json with new dependencies for dotenv, knex, sqlite3, and zod.
    - Added a new script to run knex migrations.

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,16 @@
 import fastify from 'fastify'
-
+import { knex } from './database.js'
+import { randomUUID } from 'node:crypto'
 const app = fastify()
 
-app.get('/hello', () => {
-  return 'hello world'
+app.get('/hello', async () => {
+  const transaction = await knex('transactions').insert({
+    id: randomUUID(),
+    title: 'New transaction',
+    amount: 100,
+  }).returning('*')
+
+  return transaction
 })
 
 app
